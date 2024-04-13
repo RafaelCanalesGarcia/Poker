13a1c65be3a48b38b0ea660339d8b10a68d23e20
RafaCanales <rafael.s372746@cesurformacion.com>
2024-04-14 00:04:53 +0200

    Empezamos person y money

---
+++ src/poker/dominio/money.java
@@ -0,0 +1,19 @@
+package poker.dominio;
+
+public class money {
+    private int bet;
+    private int quantity;
+
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getBet() {
+        return bet;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
